8c888d8327664eb8326757e5feceff311e705188
Bjorn André Foss <andrecfoss@outlook.com>
2024-05-21 18:54:54 +0100

    Add files via upload

---
+++ BlazorChat/BlazorChat/Shared/CommentsMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorChat.Shared
+{
+    public class CommentsMessage
+    {
+
+        public long Id { get; set; }
+        public long PostMessageId { get; set; }
+        public string FromUserId { get; set; }
+        public string UserName { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+
+        public virtual PostMessage PostMessage { get; set; }
+
+
+    }
+}
