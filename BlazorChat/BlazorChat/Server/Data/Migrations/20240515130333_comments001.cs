2b0cf0b365dc9e4d4c1556edd92052565d4289a0
Bjorn André Foss <andrecfoss@outlook.com>
2024-05-21 19:07:45 +0100

    Add files via upload

---
+++ BlazorChat/BlazorChat/Server/Data/Migrations/20240515130333_comments001.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorChat.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class comments001 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ BlazorChat/BlazorChat/Server/Data/Migrations/20240515130704_comments003.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorChat.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class comments003 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PostMessages",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FromUserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostMessages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CommentsMessages",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PostMessageId = table.Column<long>(type: "bigint", nullable: false),
+                    FromUserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CommentsMessages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CommentsMessages_PostMessages_PostMessageId",
+                        column: x => x.PostMessageId,
+                        principalTable: "PostMessages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommentsMessages_PostMessageId",
+                table: "CommentsMessages",
+                column: "PostMessageId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CommentsMessages");
+
+            migrationBuilder.DropTable(
+                name: "PostMessages");
+        }
+    }
+}
